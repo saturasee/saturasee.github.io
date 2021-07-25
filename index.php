5741951b0a560f3a245a41502556fe6da4f66871
Kreateev <kreateev666@gmail.com>
2021-07-26 00:47:11 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+		<title>SATURASEE</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="col-xs-12">
+				<div class="text-center" style="padding-top: 30px; padding-bottom: 30px;">
+					<img class="backdrop linktree">
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="col-xs-12">
+				<div class="text-center">
+					<div style="padding-bottom: 30px;">
+						<button onclick="location.href='https://instagram.com/saturasee'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">Instagram</button>
+					</div>
+					<div style="padding-bottom: 30px;">
+						<button onclick="location.href='https://tiktok.com/@saturasee'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">TikTok</button>
+					</div>
+					<div style="padding-bottom: 30px;">
+						<button onclick="location.href='https://www.youtube.com/channel/UCNOpsqVN7w_cr1_r3PzmlJQ'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">Youtube</button>
+					</div>
+					<div style="padding-bottom: 30px;">
+						<button onclick="location.href='https://facebook.com/saturasee'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">Facebook</button>
+					</div>
+					<div style="padding-bottom: 30px;">
+						<button onclick="location.href='https://twitter.com/saturasee'" type="button" class="btn btn-outline-light" style="width: 80%; padding-top:10px; padding-bottom:10px; font-weight: 600;">Twitter</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="jquery-3.3.1.slim.min.js"></script>
+	</body>
+</html>
